dd9bdc9693ccbe96315f3289e43a19112c8de4b0
satyam gupta <satyamgupta2997@gmail.com>
2023-06-15 15:00:05 +0530

    desktop UI completion
    desktop UI completion

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,35 @@ import './App.scss';
 
 function App() {
 	return (
-		<article>
+		<article className='calc-container'>
 			<div className='calc-wrapper'>
 				<div className='calc-input-container'>
 					<div className='date-inputs'>
 						<label htmlFor='day'>Day</label>
-						<input type='text' id='day' className='input-field' />
+						<input
+							type='text'
+							id='day'
+							className='input-field'
+							placeholder='DD'
+						/>
 					</div>
 					<div className='date-inputs'>
 						<label htmlFor='month'>Month</label>
-						<input type='text' id='month' className='input-field' />
+						<input
+							type='text'
+							id='month'
+							className='input-field'
+							placeholder='MM'
+						/>
 					</div>
 					<div className='date-inputs'>
 						<label htmlFor='year'>Year</label>
-						<input type='text' id='year' className='input-field' />
+						<input
+							type='text'
+							id='year'
+							className='input-field'
+							placeholder='YYYY'
+						/>
 					</div>
 				</div>
 				<div className='calc-mid-container'>
@@ -25,6 +40,23 @@ function App() {
 							<img src='/icon-arrow.svg' alt='down arrow' />
 						</button>
 					</div>
+				</div>
+				<div className='calc-result-container'>
+					<p className='result-data'>
+						<i>
+							<span className='age-number'>--</span> years
+						</i>
+					</p>
+					<p className='result-data'>
+						<i>
+							<span className='age-number'>--</span> months
+						</i>
+					</p>
+					<p className='result-data'>
+						<i>
+							<span className='age-number'>--</span> days
+						</i>
+					</p>
 				</div>
 			</div>
 		</article>
